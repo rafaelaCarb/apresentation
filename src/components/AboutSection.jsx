cc9795c766c4b1a3fd2aed4b299910cef8bed9fc
Rafaela Carbelim Bandeira <134436874+rafaelaCarb@users.noreply.github.com>
2024-11-11 07:18:57 -0300

    project-section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -8,7 +8,7 @@ const TAB_DATA = [
     title: "Habilidades",
     id: "skills",
     content: (
-      <ul>
+      <ul className="list-disc pl-2">
         <li>CSS</li>
         <li>Tailwind</li>
         <li>React</li>
@@ -20,14 +20,25 @@ const TAB_DATA = [
   {
     title: "Educação",
     id: "education",
-    content:
-      "Atualmente, estou no quarto período da faculdade e continuo me dedicando a aprender e aprimorar minhas habilidades.",
+    content: (
+      <ul className="list-disc pl-1">
+        <p>
+          Atualmente, estou no quarto período da faculdade e continuo me
+          dedicando a aprender e aprimorar minhas habilidades.
+        </p>
+      </ul>
+    ),
   },
   {
     title: "Experiência",
     id: "experience",
-    content:
-      "Sou desenvolvedora front-end, com aproximadamente 8 meses de experiência focada no desenvolvimento de interfaces interativas e responsivas. Gosto de trabalhar em equipe e estou sempre em busca de novos desafios para criar aplicações inovadoras e funcionais.",
+    content: (
+      <ul className="list-disc pl-1">
+        <p>
+        Sou desenvolvedora front-end, com aproximadamente 8 meses de experiência focada no desenvolvimento de interfaces interativas e responsivas.
+        </p>
+      </ul>
+    ),
   },
 ];
 const AboutSection = () => {
@@ -42,7 +53,7 @@ const AboutSection = () => {
 
   return (
     <section className="text-white">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+      <div className="lg:grid lg:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image
           src={"/images/about-image.png"}
           width={500}
@@ -50,7 +61,7 @@ const AboutSection = () => {
           alt="About Us"
           className="rounded-lg"
         />
-        <div className="mt-4 md:mt-0 text-left h-full flex flex-col">
+        <div className="mt-4 lg:mt-0 text-left h-full flex flex-col">
           <h2 className="text-4xl font-bold text-white mb-4">Sobre mim</h2>
           <p className="text-base md:text-lg ">
             Sou desenvolvedora front-end, com aproximadamente 8 meses de
@@ -60,13 +71,12 @@ const AboutSection = () => {
             Postman. No back-end, possuo conhecimento em Node.js e já estudei
             Java com Spring Boot e MySQL. Atualmente, estou no quarto período da
             faculdade e continuo me dedicando a aprender e aprimorar minhas
-            habilidades. Gosto de trabalhar em equipe e estou sempre em busca de
-            novos desafios para criar aplicações inovadoras e funcionais.
+            habilidades.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTab("skills")}
-              active={tab === "skils"}
+              active={tab === "skills"}
             >
               {" "}
               Habilidades{" "}
--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -1,11 +1,14 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+
 const projectsData = [
   {
     id: 1,
     title: "React Portfolio Website",
     description: "Project 1 description",
-    image: "../../public/images/projects/1.png",
+    image: "/images/projects/1.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
@@ -14,7 +17,7 @@ const projectsData = [
     id: 2,
     title: "Potography Portfolio Website",
     description: "Project 2 description",
-    image: "../../public/images/projects/2.png",
+    image: "/images/projects/2.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
@@ -23,7 +26,7 @@ const projectsData = [
     id: 3,
     title: "E-commerce Application",
     description: "Project 3 description",
-    image: "../../public/images/projects/3.png",
+    image: "/images/projects/3.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
@@ -32,7 +35,7 @@ const projectsData = [
     id: 4,
     title: "Food Ordering Application",
     description: "Project 4 description",
-    image: "../../public/images/projects/4.png",
+    image: "/images/projects/4.png",
     tag: ["All", "Mobile"],
     gitUrl: "/",
     previewUrl: "/",
@@ -41,7 +44,7 @@ const projectsData = [
     id: 5,
     title: "React Firebase Template",
     description: "Authentication and CRUD operations",
-    image: "../../public/images/projects/5.png",
+    image: "/images/projects/5.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
@@ -50,26 +53,54 @@ const projectsData = [
     id: 6,
     title: "Full-stack Roadmap",
     description: "Project 5 description",
-    image: "../../public/images/projects/6.png",
+    image: "/images/projects/6.png",
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 const ProjectSection = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  )
+
   return (
     <div>
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="gris md:grid-cols-3 gap-8 md:gap-12">
-        {projectsData.map((project) => (
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
-            tags={project}
+            gitUrl={project.gitUrl}
+            previweUrl={project.previewUrl}
           />
         ))}
       </div>
